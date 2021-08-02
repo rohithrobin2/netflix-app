b40ee3e514ade3faecbbe4184bcfaedc4599a92f
rohithrobin2 <rohithrobin2@gmail.com>
2021-08-02 14:08:42 +0000

    first commit

---
+++ src/Row.js
@@ -0,0 +1,72 @@
+import axios from './axios';
+import React, { useEffect, useState } from 'react'
+import './Row.css'
+import { ThumbUpSharp } from '@material-ui/icons';
+
+
+function Row({title,fetchUrl,isLargeRow}) {
+    const [movies,setMovies]=useState([]);
+    const base_url="https://image.tmdb.org/t/p/original/";
+
+    useEffect(()=>{
+async function fetchData(){
+    const request =await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+        }
+        fetchData();
+
+    },[fetchUrl])
+
+console.log(movies)
+function truncate(string,n){
+    return string?.length>n ? string.substr(0,n-1) + '...' :string;
+    
+        } 
+    return (
+        <div className='row' >
+            <h2>{title}</h2>
+           
+        <div className="row_posters ">
+            {movies.map(
+                (movie) =>
+
+                (((isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path ))&&(
+                
+                <div className="group">
+                <img 
+                
+                className={`row_poster ${ isLargeRow &&"row_posterLarge"}`}
+                
+                key={movie.id}
+                src={`${base_url}${
+                    isLargeRow ? movie.poster_path: movie.backdrop_path
+                }`} alt={movie.name} 
+               
+                />
+                <div className="movie_details " >
+                  
+                  
+                  <h2 style={{minWidth:"100px"}} >  {movie?.title||movie.name||movie?.original_name}</h2>  
+
+                  <h5 style={{}}>Rating:{movie.vote_average} &nbsp;
+                   Date: {movie.release_date||movie.first_air_date}</h5>
+                  
+                  <p>{truncate( movie?.overview,50)}</p>
+                  <p>< ThumbUpSharp/> {movie.vote_count}</p>
+                  </div>
+          </div>
+                
+                     ) ))
+               
+                }
+             
+            
+            </div>
+         
+        </div>
+    )
+}
+
+export default Row;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDl9JOkt1lhYT683diJL0uUxRmnVzTXTUc",
+    authDomain: "netflix-b532c.firebaseapp.com",
+    projectId: "netflix-b532c",
+    storageBucket: "netflix-b532c.appspot.com",
+    messagingSenderId: "453365941479",
+    appId: "1:453365941479:web:644b2293965d1c266e4e42"
+  };
